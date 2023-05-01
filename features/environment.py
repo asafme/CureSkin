286081f84fbd803c7ddd42cc06f85f7e5f08d721
asafme <jessicaburgh@gmail.com>
2023-05-01 18:18:09 -0400

    task 3

--- features/environment.py
+++ features/environment.py
@@ -1,15 +1,45 @@
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
+from service import Service
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
 from app.application import Application
+
 
 def browser_init(context):
     """
     :param context: Behave context
     """
+    # ############# CHROME ############
+    # options = ChromeOptions()
+    # options.add_argument("--headless")
+    # options.add_argument("--window-size=1920,1080")
+    # options.add_argument("--start-maximized")
+    # context.driver = webdriver.Chrome(executable_path="../chromedriver", options=options)
+    # #################################
 
-    context.driver = webdriver.Chrome(executable_path="../chromedriver")
+    ######### FIREFOX ##############
+    # options = FirefoxOptions()
+    # options.headless = True
+    # context.driver = webdriver.Firefox(executable_path='geckodriver', options=options)
+    # ################################
+
+    # service = Service('/Users/jessicamenashe/asafme/CureSkin/chromedriver')
+
     # context.browser = webdriver.Safari()
     # context.browser = webdriver.Firefox()
+
+    # ## HEADLESS MODE ####
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # # options.add_argument("--window-size=1920,1080")
+    # # options.add_argument("--sta
+    #
+    # context.driver = webdriver.Chrome(
+    #     chrome_options=options,
+    #     service=Service('/Users/jessicamenashe/asafme/CureSkin/chromedriver')
+    # )
 
     context.driver.wait = WebDriverWait(context.driver, 10)
     context.driver.maximize_window()
