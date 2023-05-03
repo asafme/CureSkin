fc5c143b5f984f75610f047a643046c56afa9daf
asafme <jessicaburgh@gmail.com>
2023-05-03 12:35:46 -0400

    Task 5

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,17 @@
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from service import Service
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
+import allure
+from allure_commons.types import AttachmentType
+from behave.configuration import options
 
 from app.application import Application
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from app.application import Application
+from selenium.webdriver.support.events import EventFiringWebDriver
+from selenium.webdriver.chrome.service import Service
+
+from support.logger import logger, MyListener
 
 
 def browser_init(context):
@@ -25,8 +32,8 @@ def browser_init(context):
     # context.driver = webdriver.Firefox(executable_path='geckodriver', options=options)
     # ################################
 
-    # service = Service('/Users/jessicamenashe/asafme/CureSkin/chromedriver')
-
+    service = Service('/Users/jessicamenashe/asafme/CureSkin/chromedriver')
+    context.driver = webdriver.Chrome(service=service)
     # context.browser = webdriver.Safari()
     # context.browser = webdriver.Firefox()
 
@@ -42,16 +49,16 @@ def browser_init(context):
     # )
 
     ###### for browserstack ########
-    desired_cap = {
-        'browser': 'Firefox',
-        'os_version': '11',
-        'os': 'Windows',
-        # 'name': test_name
-    }
-    bs_user = 'jessicamenashe_zDS1Ke'
-    bs_key = 'HFiUD7M9VxyaRXsHEfkS'
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
+    # desired_cap = {
+    #     'browser': 'Firefox',
+    #     'os_version': '11',
+    #     'os': 'Windows',
+    #     # 'name': test_name
+    # }
+    # bs_user = 'jessicamenashe_zDS1Ke'
+    # bs_key = 'HFiUD7M9VxyaRXsHEfkS'
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
 
 
     context.driver.wait = WebDriverWait(context.driver, 10)
@@ -63,15 +70,21 @@ def browser_init(context):
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
     browser_init(context)
+    # print('\nStarted scenario: ', scenario.name)
+    logger.info(f'Started scenario: {scenario.name}')
+    # browser_init(context, scenario.name)
 
 
 def before_step(context, step):
     print('\nStarted step: ', step)
+    # print('\nStarted step: ', step)
+    logger.info(f'Started step: {step}')
+
 
 
 def after_step(context, step):
-    if step.status == 'failed':
-        print('\nStep failed: ', step)
+    logger.error(f'Step failed: {step}')
+    print('\nStep failed: ', step)
 
 
 def after_scenario(context, feature):
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-
+from support.logger import logger
 from pages.base_page import Page
 
 
@@ -7,6 +7,7 @@ class MainPage(Page):
     ADD_TO_CART = (By.CSS_SELECTOR, ".product-form__submit")
     INCREASE_QUANTITY = (By.XPATH, "//button[@class='quantity__button no-js-hidden']")
     def open_main_url(self):
+        logger.info('Opening url https://shop.cureskin.com/...')
         self.open_url('https://shop.cureskin.com/')
 
 
